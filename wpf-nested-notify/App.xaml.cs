84de25109038eb9fa705217d5614aeeb8b3dc36d
Tom Gregor <tgregor@ivsoftware.com>
2024-09-29 12:17:56 -0600

    Add project files.

---
+++ wpf-nested-notify/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace wpf_nested_notify
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ wpf-nested-notify/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+
+namespace wpf_nested_notify
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow() => InitializeComponent();
+    }
+    partial class MainPageViewModel : ObservableObject
+    {
+        public MainPageViewModel()
+        {
+            ButtonClickedCommand = new RelayCommand(() =>
+            {
+                _count++;
+                string plural = _count > 1 ? "s" : string.Empty;
+                Text = $"You clicked {_count} time{plural}";
+            });
+        }
+        int _count = 0;
+        public ICommand ButtonClickedCommand { get; }
+
+        [ObservableProperty]
+        string _text = "Click Me";
+
+        // This 'is' an ObservableObject because it provides INotifyPropertyChanged.
+        // It 'is not' an ObservableProperty however, unless you're swapping out settings
+        // en masse e.g. because you have Profiles with their own individual Settings.
+        // ==================================================================================
+        public SettingsClass Settings { get; } = new SettingsClass(); // The "one and only" settings object.
+    }
+    partial class SettingsClass : ObservableObject
+    {
+        [ObservableProperty]
+        bool _useItalic;
+
+        [ObservableProperty]
+        FontStyle _fontSetting;
+
+        protected override void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            base.OnPropertyChanged(e);
+            switch (e.PropertyName)
+            {
+                case nameof(UseItalic):
+                    FontSetting = UseItalic ? FontStyles.Italic : FontStyles.Normal;
+                    break;
+            }
+        }
+    }
+}
